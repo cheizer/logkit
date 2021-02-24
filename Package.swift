8c0fb6ae6124d540a9d50975f3dfd8928d1b571d
Charles Heizer <heizer1@llnl.gov>
2021-02-24 09:43:00 -0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -37,11 +37,12 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "LogKit",
-            dependencies: []),
+            dependencies: [],
         ),
         .testTarget(
             name: "LogKitTests",
-            dependencies: ["LogKit"]),
+            dependencies: ["LogKit"]
+        )
     ],
     swiftLanguageVersions: [.v5]
 )
