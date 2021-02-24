926359cb1cdc2916a5e03c7b4043bd36f97339e4
Charles Heizer <heizer1@llnl.gov>
2021-02-24 09:46:07 -0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -28,21 +28,5 @@ let package = Package(
         .library(
             name: "LogKit",
             targets: ["LogKit OSX","LogKit iOS","LogKit tvOS","LogKit watchOS"])
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "LogKit",
-            dependencies: []
-        )
-        .testTarget(
-            name: "LogKitTests",
-            dependencies: ["LogKit"]
-        )
-    ],
-    swiftLanguageVersions: [.v5]
+    ]
 )
