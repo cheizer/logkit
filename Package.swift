a5fa81c92170119b170c957cbeaf319a2207432c
Charles Heizer <heizer1@llnl.gov>
2021-02-24 09:57:06 -0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,3 @@
-// swift-tools-version:5.3
 // Package.swift
 //
 // Copyright (c) 2015 - 2016, Justin Pawela & The LogKit Project
@@ -19,25 +18,5 @@
 import PackageDescription
 
 let package = Package(
-    name: "LogKit",
-    platforms: [
-        .macOS(.v10_13), .iOS(.v13), .tvOS(.v13)
-    ],
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "LogKit",
-            targets: ["LogKit"]
-        )
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "LogKit"
-        )
-    ]
+    name: "LogKit"
 )
