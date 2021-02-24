1970b8baaaafad1ed2685ab6129eba415bde26c5
Charles Heizer <heizer1@llnl.gov>
2021-02-24 10:03:31 -0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,22 +1,30 @@
-// Package.swift
-//
-// Copyright (c) 2015 - 2016, Justin Pawela & The LogKit Project
-// http://www.logkit.info/
-//
-// Permission to use, copy, modify, and/or distribute this software for any
-// purpose with or without fee is hereby granted, provided that the above
-// copyright notice and this permission notice appear in all copies.
-//
-// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-// WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-// MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-// ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-// WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-// ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-// OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
-    name: "LogKit"
+    name: "LogKit",
+    platforms: [
+        .macOS(.v10_13), .iOS(.v13), .tvOS(.v13)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "LogKit",
+            targets: ["LogKit"])
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "LogKit",
+            exclude: [],
+            resources: []
+        ),
+        .testTarget(
+            name: "LogKitTests",
+            dependencies: ["LogKit"]),
+    ]
 )
