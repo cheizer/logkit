7c17bee520a2468ae54a69d72ce079d0be7d3f2f
Charles Heizer <heizer1@llnl.gov>
2021-02-24 09:47:37 -0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -22,9 +22,5 @@ let package = Package(
     name: "LogKit",
     platforms: [
         .macOS(.v10_12), .iOS(.v13), .tvOS(.v13)
-    ],
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library( name: "LogKit" )
     ]
 )
