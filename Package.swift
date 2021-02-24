d0a05322e06d18cee004eb925fd2bfdab4ab59a2
Charles Heizer <heizer1@llnl.gov>
2021-02-24 09:44:18 -0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -37,8 +37,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "LogKit",
-            dependencies: [],
-        ),
+            dependencies: []
+        )
         .testTarget(
             name: "LogKitTests",
             dependencies: ["LogKit"]
